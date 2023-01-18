bc15bef068496e4cf0e578a9855755fe27d25619
mcchris1 <121463561+mcchris1@users.noreply.github.com>
2023-01-18 09:16:39 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Michael is the champion";
